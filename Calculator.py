8d0823726001995bcd1d0b7c4055a9d7ae13abf3
Samyak Jain <137718415+jainsamyak01@users.noreply.github.com>
2024-06-17 16:31:22 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+x = ("This is a normal calculator made using python by @samyak jain")
+print(x.center(150))
+num1 = int(float(input("Enter a number for calculation: ")))
+num2 = int(float(input("Enter another number for calculation: ")))
+add = num1 + num2
+sub = num1 - num2
+division = num1 / num2
+multiplication = num1 * num2
+print("The sum of two number is: ", add)
+print("The sub of two number is: ", sub)
+print("The divison of two number is: ", division)
+print("The multiplication of two number is: ", multiplication)
